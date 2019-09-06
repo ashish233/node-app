6edd06bc9ff49a1c91c32b4790f6df6724d76366
ashish233 <pandeashish233@gmail.com>
2019-09-06 19:35:29 +0530

    basic code added

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
+//importing node framework
 var express = require('express');
-
+ 
 var app = express();
-
-
-
-app.get('/',function(req,res){
-
-res.send("hello world");
-
+//Respond with "hello world" for requests that hit our root "/"
+app.get('/', function (req, res) {
+ res.send('hello world');
 });
-
-
-app.listen(3000,function(){
-  console.log("listening port 3000")
-})
+//listen to port 3000 by default
+app.listen(process.env.PORT || 3000);
+ 
+module.exports = app;
